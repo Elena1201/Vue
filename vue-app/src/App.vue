42ec30d4e9c9174fa675a100a965219c6335e27c
elena <jessie84121@gmail.com>
2022-10-21 17:16:59 +0800

    yu

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,31 +1,16 @@
 <script setup>
-import HelloVue from '@/components/HelloVue.vue'
-import Nav from './components/nav.vue'
+import HelloVue from "@/components/HelloVue.vue";
+import Nav from "./components/Nav.vue";
 </script>
 
 <template>
   <Nav></Nav>
   <div class="row">
     <div class="col-3"><HelloVue></HelloVue></div>
-      <div class="col-6">
-        <router-view></router-view>
-      </div>
+    <div class="col-6">
+      <router-view></router-view>
+    </div>
   </div>
-
-
-
-
-
-
-
-
-  <!-- <router-link to="/">Home</router-link> 
-  <router-link to="/about">About</router-link>
-  <hr />
-  <router-view />
-    <div class="col-3"><HelloVue></HelloVue></div> -->
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- vue-app/src/router.js
+++ vue-app/src/router.js
@@ -1,28 +1,28 @@
-import { createWebHistory, createRouter } from 'vue-router';
-import Home from '@/views/Home.vue'
-import About from './views/About.vue'
-import Todo from './views/Todo.vue'
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "@/views/Home.vue";
+import About from "./views/About.vue";
+import Todo from "./views/Todo.vue";
 
-const routes =[
-    {
-        path:"/",
-        name:"Home",
-        component:Home
-    },
-    {
-        path:"/about",
-        name:"About",
-        component:About
-    },
-    {
-        path:"/todo",
-        name:"Todo",
-        component:Todo
-    }
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+  {
+    path: "/todo",
+    name: "Todo",
+    component: Todo,
+  },
 ];
 const router = createRouter({
-    history: createWebHistory(),
-    routes //routes:routes
-    });
-    export default router;
-    
+  history: createWebHistory(),
+  routes, //routes:routes
+});
+
+export default router;
